c211174b65e13436af9a2e09fd1c55f47a550774
Enderlein <31358215+Enderlein@users.noreply.github.com>
2018-01-13 16:01:30 -0500

    Added main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+A script for automatically filling in the metadata of .mp3 files
+(Fills in artist, song name, album name, and track number)
+"""
+
+from lime import *
+
+root = tk.Tk()
+root.title("LIME 0.1")
+app = MainWindow(parent=root)
+app.mainloop()
+                
+__author__ = "Kevin Guzman"
+__copyright__ = "Copyright 2017, Kevin Guzman"
+__credits__ = ["Kevin Guzman"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Kevin Guzman"
+__email__ = "h.keving81@gmail.com"
+__status__ = "Development"
